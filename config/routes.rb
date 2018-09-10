21c793d71a34a2fd5ddd220712936fc0e1162702
Apollo725 <natashazaharova725@outlook.com>
2018-09-10 19:34:18 +1000

    Controller Setup and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :books
-  resources :library, only[:index]
-  devise_for :users
+  resources :books do
+    member do
+      put "add", to: "books#library"
+      put "remove", to: "books#library"
+    end
+  end
+
+  resources :library, only:[:index]
+  resources :pricing, only:[:index]
+  devise_for :users, controllers: {registrations: "registrations"}
   root to: 'books#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :subscriptions
 end
